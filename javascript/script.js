b8a73d82deda68066504b474b2bcadb18b4f3985
michele camera <michy.cam95@gmail.com>
2022-04-05 09:52:39 +0200

    modify

--- javascript/script.js
+++ javascript/script.js
@@ -7,12 +7,13 @@ const app = new Vue({
     emails:[],
     },
     mounted(){
+      for(let i = 0; i <10; i++){
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( (response) => {
           const result = response.data;
           this.emails.push(result.response);
-
           console.log(result);
           console.log(this.emails);
         });
+      }           
     } 
 })
